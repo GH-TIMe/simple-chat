3cde255e47af46a8a0018ceed7ee1a1c113e9f52
Tim <timhaha@yandex.ru>
2020-10-19 21:59:09 +0300

    Updates

---
+++ precache-manifest.4809fe7b2b59d29aaf7ce39caef4d7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2855d21c2769d3173a4a3fcd38dbf4",
+    "url": "/simple-chat/index.html"
+  },
+  {
+    "revision": "290db4a7bfb3db15a334",
+    "url": "/simple-chat/static/css/main.a798c3d7.chunk.css"
+  },
+  {
+    "revision": "799435ad40c6bdde60d9",
+    "url": "/simple-chat/static/js/2.17bf7f5b.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/simple-chat/static/js/2.17bf7f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290db4a7bfb3db15a334",
+    "url": "/simple-chat/static/js/main.aa52443f.chunk.js"
+  },
+  {
+    "revision": "d8ad321354e21979292c",
+    "url": "/simple-chat/static/js/runtime-main.9e7381f1.js"
+  }
+]);
